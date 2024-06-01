c04502dbd3367a832ea28f60f7503010b7e62b1d
CpMihai <capy.michael@gmail.com>
2024-06-01 08:19:58 +0000

    Class definition

--- src/Member.php
+++ src/Member.php
@@ -1,1 +1,22 @@
 <?php
+
+class Member
+{
+    public string $username;
+    public string $role;
+    const ADMIN_ROLE = "admin";
+    const DEFAULT_ROLE = "member";
+
+    public function addWorkspaceMember()
+    {
+    }
+    public function createChat()
+    {
+    }
+    public function createWorkspace()
+    {
+    }
+    public function postMessageToChat()
+    {
+    }
+}
--- src/Workspace.php
+++ src/Workspace.php
@@ -1,1 +1,10 @@
 <?php
+
+class Workspace
+{
+    public array $members = [];
+    public array $chats = [];
+    public string $url;
+
+    public static string $urlDomain = ".flack.app";
+}
