57e49cb0e5e20b0256ea3ef6d4e4126eede8cb9f
CpMihai <capy.michael@gmail.com>
2024-06-01 18:20:11 +0000

    members

--- src/app.php
+++ src/app.php
@@ -3,5 +3,16 @@
 include '_includes.php';
 
 $admin = new Member();
+$admin->username = "Mihai";
+$workspace = $admin->createWorkspace("www.linkedin.com");
 
 $member = new Member();
+$member->username = "Mirela";
+
+
+$chat = $admin->createChat("title chat", $workspace);
+$chat_member = $member->createChat("titsle chat", $workspace);
+$workspace->setAdmin($admin);
+$admin->addWorkspaceMember($member, $workspace);
+
+$member->postMessageToChat("test", $chat);
